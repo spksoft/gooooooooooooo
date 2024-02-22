30f034428d0354fdcad70cc1d4acb70b790e05bd
spksoft <spkrsk.37@gmail.com>
2024-02-22 10:30:35 +0700

    Add middleware and hash packages***
    ***Delete chat and gift packages***
    
    ***Create hash package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/spksoft/gooooooooooooo/cmd/api/internal/middleware"
+import (
+	"github.com/spksoft/gooooooooooooo/cmd/api/internal/middleware"
+	"github.com/spksoft/gooooooooooooo/pkg/hash"
+)
 
 func main() {
 	middleware.New()
+	hash.New()
 }
--- pkg/common/gift/gift.go
+++
@@ -1,3 +0,0 @@
-package gift
-
-func New() {}
